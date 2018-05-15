210f01cd0a20dab98b60710d96ec44b27a0a0912
Kierancz <kieran.czerwinski@colorado.edu>
2018-05-15 15:44:55 -0600

    Add crawler fields and search only sitemap

--- indexer/items.py
+++ indexer/items.py
@@ -12,4 +12,15 @@ class IndexerItem(scrapy.Item):
     # The source URL
     url_from = scrapy.Field()
     # The destination URL
-    url_to = scrapy.Field()
+    # url_to = scrapy.Field()
+
+    # Meta tags
+    title = scrapy.Field()
+    description = scrapy.Field()
+    keywords = scrapy.Field()
+
+    # Body tags
+    h1 = scrapy.Field()
+    h2 = scrapy.Field()
+    h3 = scrapy.Field()
+    p = scrapy.Field()
